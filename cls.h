fb0ed31d6e6813875ecf9f5abe5f1f1f76e39694
WashingtonD <68158381+WashingtonD@users.noreply.github.com>
2021-12-09 12:23:13 +0100

    ClearScreen ver.2
    Second version of the ClearScreen function (Which changes console buffer)

--- cls.h
+++ cls.h
@@ -3,6 +3,11 @@
 
 using namespace std;
 
+const int col = 80; /// Constant variables which are used for the cls2 function
+const int row = 50;
+
+
+
 
 int cls()
 {
@@ -28,4 +33,28 @@ int cls()
         return 0;
     else
         return 1;
+}
+
+
+void cls2(HANDLE wHnd) /// Getting console Handle as a argument of cls2 function
+{
+	CHAR_INFO consBuffer[col * row]; /// Creating character information buffer; (Containing symbols and other info about characters)
+
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	GetConsoleScreenBufferInfo(wHnd, &csbi); /// Getting standart console parameters into csbi variable
+
+	for (int i = 0; i < col * row; i++) /// Col & row here - are constant representation of console buffer size
+	{
+		consBuffer[i].Char.UnicodeChar = ' ';			/// setting every character in the buffer as an empty UNICODE symbol (space)
+		consBuffer[i].Attributes = csbi.wAttributes;   ///  setting all the other attributes of this symbol as the console default one
+	}
+
+	COORD charPosition = { 0,0 }; /// Setting starting point of the console
+	SMALL_RECT writeArea = { 0,0,col - 1,row - 1 };	/// Setting which area is going to be changed in the clearence proccess
+	COORD bufferSize = { col,row }; /// getting out buffer size as a COORD;
+	WriteConsoleOutput(wHnd, /// Console Handle
+						consBuffer, ///  Buffer with the data to be written
+						bufferSize,  /// Size of the consBuffer  
+						charPosition, /// Coordinate of the left-upper point of the buffer
+						&writeArea);  /// Left-upper and right-lower points of the console screen buffer rect to witrren to
 }
